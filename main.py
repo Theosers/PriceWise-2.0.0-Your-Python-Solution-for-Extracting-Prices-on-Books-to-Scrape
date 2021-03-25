500a314544a0f8888dc8bb710641334d3c1c0a3c
theosers <theophile.sers@yahoo.com>
2021-03-25 16:44:32 +0100

    Tout les livres d'une catégorie

--- main.py
+++ main.py
@@ -3,13 +3,43 @@ from bs4 import BeautifulSoup
 import csv
 import module
 
-url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-reponse = requests.get(url)
+links = []
+livres = []
+
+url_page = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
+
+reponse = requests.get(url_page)
 if reponse.ok:
     soup = BeautifulSoup(reponse.text, 'html.parser')
+    articles = soup.findAll('article')
 
-    livre1 = module.livre(soup,'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
-    # print(livre1.infos)
-    livre1.save_CSV()
+    for article in articles:
+        a = article.find('a')
+        link = a['href']
+        link = link.replace('../../../', '')
+        links.append('http://books.toscrape.com/catalogue/' + link)
+else:
+    print("Vous n'avez pas accès à cette page")
+
+
+
+
+
+
+for n in range(len(links)):
+
+    url = links[n]
+
+    reponse = requests.get(url)
+    if reponse.ok:
+        soup = BeautifulSoup(reponse.text, 'html.parser')
+
+
+        livres.append(module.livre(soup, url))
+
+for a in livres:
+    print(a.infos)
+
+        #livre1.save_CSV()
 
 
