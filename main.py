fcdba2716d5db5bc59ce26fd9e10c1e76e078105
theosers <theophile.sers@yahoo.com>
2021-03-28 16:40:34 +0200

    Version POO fni

--- main.py
+++ main.py
@@ -6,17 +6,36 @@ import fonction
 import re
 
 
-url_categorie = 'http://books.toscrape.com/'
+extraire_PagePrincipal = module.extract('http://books.toscrape.com/index.html')
+extraire_PagePrincipal.get_soup()
 
-categories = fonction.All_categories(url_categorie)
-
-url_page = fonction.category_pages_url(url_categorie, categories) #récupère tout les urls de toute les pages d'une catégorie
-
-fonction.get_full_category_bookslink(url_page) #récupère tout les liens des livres de toute les pages d'une catégorie
-
-fonction.get_infos_book(links) # récupère les informations de tout les livres
+Traiter_PagePrincipal = module.Traitement(extraire_PagePrincipal.soup)
 
 
+for counter, category in enumerate(Traiter_PagePrincipal.traitement_soup_PagePrincipal()):
+
+    extraire_category = module.extract(category)
+    extraire_category.get_soup()
+    Traiter_category = module.Traitement(extraire_category.soup)
 
 
+    for page in range(1, int(Traiter_category.number_of_page()) +1 ):
 
+        if page == 1 :
+            extraire_page = module.extract(extraire_category.url+ 'index' + '.html')
+        else :
+            extraire_page = module.extract(extraire_category.url+ 'page-' + str(page) + '.html')
+
+
+        extraire_page.get_soup()
+        Traiter_category = module.Traitement(extraire_page.soup)
+
+
+        for counter,book in enumerate(Traiter_category.traitement_soup_category()):
+
+            extraire_livre = module.extract(book)
+            extraire_livre.get_soup()
+
+            Traiter_livre = module.Traitement(extraire_livre.soup)
+
+            module.save(Traiter_livre.traitement_soup_livres(book))
--- module.py
+++ module.py
@@ -2,49 +2,108 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import os
+import re
 
-class livre:
 
-    def __init__(self, soup, url):
-        self.product_page_url = url
-        self.universal_product_code = soup.findAll('td')[0].string
-        self.title = soup.findAll('h1')[0].string
-        self.price_including_tax = soup.findAll('td')[3].string
-        self.prince_excluding_tax = soup.findAll('td')[2].string
-        self.number_available = soup.findAll('td')[5].string
-        self.product_description = soup.findAll('p')[3].string
-        self.category = soup.findAll('a')[3].string
-        self.review_rating = soup.findAll('td')[6].string
-        self.image_url = soup.findAll('img')[0]['src']
-        #        self.image_url = 'books.toscrape.com/' + image_url.replace('../../', '')
+class extract :
 
-        self.infos = [self.product_page_url, self.universal_product_code, self.title, self.price_including_tax,
-                      self.prince_excluding_tax,
-                      self.number_available, self.product_description, self.category, self.review_rating,
-                      self.image_url]
+    def __init__(self, url):
+        self.url = url
+        self.soup = None
 
-    def save_CSV(self):
+    def get_soup(self):
+
+        reponse = requests.get(self.url)
+        if reponse.ok:
+            soup = BeautifulSoup(reponse.text, 'html.parser')
+
+        else:
+            print("non")
+        self.soup = soup
+
+
+
+class Traitement :
+
+    def __init__(self, soup):
+
+        self.soup = soup
+
+
+    def traitement_soup_livres(self, url):
+
+        product_page_url = url
+        universal_product_code = self.soup.findAll('td')[0].string
+        title = self.soup.findAll('h1')[0].string
+        price_including_tax = self.soup.findAll('td')[3].string
+        prince_excluding_tax = self.soup.findAll('td')[2].string
+        number_available = self.soup.findAll('td')[5].string
+        product_description = self.soup.findAll('p')[3].string
+        category = self.soup.findAll('a')[3].string
+        review_rating = self.soup.findAll('td')[6].string
+        image_url = self.soup.findAll('img')[0]['src']
+
+        infos = [product_page_url, universal_product_code, title, price_including_tax,
+                 prince_excluding_tax,
+                 number_available, product_description, category, review_rating,
+                 image_url]
+        return infos
+
+    def number_of_page(self):
+
+        number_of_page = re.sub('[1\D]', '', str(self.soup.findAll('li')[-2].string))
+        if number_of_page == '':
+            number_of_page = 1
+
+        print(number_of_page)
+        return number_of_page
+
+
+    def traitement_soup_category(self):
+
+
+        articles = self.soup.findAll('article')
+        Books_links = []
+
+        for article in articles:
+            a = article.find('a')
+            link = a['href']
+            link = link.replace('../../../', '')
+
+            Books_links.append('http://books.toscrape.com/catalogue/' + link)
+
+        return Books_links
+
+
+    def traitement_soup_PagePrincipal(self):
+        categories = []
+        ul = self.soup.findAll('ul')[1]
+        a = ul.findAll('a')
+        a.pop(0)
+
+        for counter, categorie in enumerate(a):
+            #name = re.sub('[\s]', '', str(categorie.string))
+            link = a[counter]['href']
+            link = link.replace('../', '')
+            link = link.replace('index.html', '')
+            link = 'http://books.toscrape.com/' + link
+            categories.append((link))  # On obtient un tuple contenant le nom de la catégorie et son lien relatif
+
+        return categories
+
+class save :
+
+    def __init__(self, infos):
+
+        self.infos = infos
 
         with open('eggs.csv', 'a', newline='') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=' ',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            if os.path.getsize("eggs.csv") == 0 :
+            if os.path.getsize("eggs.csv") == 0:
                 spamwriter.writerow(
                     ['product_page_url'] + ['universal_product_code'] + ['title'] + ['price_including_tax'] + [
                         'prince_excluding_tax'] +
                     ['number_available'] + ['product_description'] + ['category'] + ['review_rating'] + ['image_url'])
 
             spamwriter.writerow([self.infos])
-
-
-
-
-class category :
-
-    def __init__(self, soup, url):
-        self.category_url = url
-        self.number_of_pages = 0
-
-        self.books_links = 0
-
-
