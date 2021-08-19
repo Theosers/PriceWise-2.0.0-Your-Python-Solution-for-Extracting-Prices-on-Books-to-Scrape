263d63f929138b146478b8cc537f091d62170c8d
Sers Théophile <theophile.sers@yahoo.com>
2021-08-19 17:52:09 +0200

    Add files via upload

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 echo En cours d\'execution...
-python3 code/main.py
+if [ "$(uname)" == "Darwin" ]; then
+    python3 code/main.py       
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    python3 code/main.py 
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+	echo En cours d\'execution...32
+    py code/main.py 
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    py code/main.py 
+fi
