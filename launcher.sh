f9629b335716847c50f3ae719e61812a59ad96e6
Sers Théophile <theophile.sers@yahoo.com>
2021-08-29 14:19:54 +0200

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 echo En cours d\'execution...
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" == "MAC" ]; then
     python3 code/main.py       
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     python3 code/main.py 
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-	echo En cours d\'execution...32
     py code/main.py 
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     py code/main.py 
